5a0fa56fc2098d1c2083bdc8c1664cb5ad4effcd
Tao PEINADO <tao.peinado@cpe.fr>
2024-02-07 14:13:51 +0100

    nouveaux commit

---
+++ main_test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  6 16:17:14 2024
+
+@author: tao.peinado
+"""
+
+#bibliotheques utiles
+from questions_prepas import add
+
+resultat=add(2,3)
+print(resultat)
